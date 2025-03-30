0c7e247c9ca1e816ae8bc340e35bb5d1f203642c
qazx3218 <143486424+qazx3218@users.noreply.github.com>
2025-03-30 14:16:08 +0900

    Update LikeDto.java
    .

--- src/main/java/com/example/capstone2/dto/LikeDto.java
+++ src/main/java/com/example/capstone2/dto/LikeDto.java
@@ -1,4 +1,21 @@
 package com.example.capstone2.dto;
 
+import com.example.capstone2.domain.LikeType;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
 public class LikeDto {
+
+    // 요청용
+    private Long userId;           // 좋아요 누른 사람
+    private Long boardId;          // 게시글 좋아요일 경우
+    private Long likedUserId;      // 사용자 좋아요일 경우
+
+    // 응답용
+    private Long id;
+    private LocalDateTime likedAt;
 }
